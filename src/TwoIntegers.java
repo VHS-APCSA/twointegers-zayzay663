18833ee9ff5905c5a089830f3de2da322b72ff33
bob <bob@APCSA>
2018-08-31 13:07:20 -0700

    two integer "homework"

---
+++ src/TwoIntegers.java
@@ -0,0 +1,51 @@
+
+public class TwoIntegers 
+{
+	
+	private int num1;
+	private int num2;
+	
+	public TwoIntegers()
+	{
+		num1 = 0;
+		num2 = 0;
+	}
+	
+	public TwoIntegers(int num1, int num2)
+	{
+		this.num1 = num1;
+		this.num2 = num2;
+	}
+	
+	public int getNum1()
+	{
+		return num1;
+	}
+	
+	public void setNum1(int num1)
+	{
+		this.num1 = num1;
+	}
+	
+	public int getNum2()
+	{
+		return num2;
+	}
+	
+	public void setNum2(int num2)
+	{
+		this.num2 = num2;
+	}
+
+	public String arithmetic()
+	{
+		int nums = num1 + num2;
+		int nump = num1 * num2;
+		int numd = num1 / num2;
+		int numq = num1 % num2;
+		
+		return num1 " + " "=" " nums
+		
+	}
+	
+}
