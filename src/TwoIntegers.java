92b5aa1184612e235eb9252c14e01da029cfce90
bob <bob@APCSA>
2018-09-05 14:56:57 -0700

    not done yet

--- src/TwoIntegers.java
+++ src/TwoIntegers.java
@@ -42,7 +42,36 @@ public class TwoIntegers
 		return(num1 + " + " + num2 +  " = " + (num1 + num2) + "," +
 				num1 + " * " + num2 + " = " + (num1 * num2) + "," + 
 				num1 + " / " + num2 + " = " + (num1 / num2) + "," +
-				num1 + " % " + num2 + " = " + (num1 % num2) + ",");
+				num1 + " % " + num2 + " = " + ((double) num1 % num2) + ",");
+	}
+	
+	public int larger()
+	{
+		if(num1 > num2)
+		{
+			return num1;
+		}
+		else
+		{
+			return num2;
+		}
+	}
+	
+	public boolean isEven()
+	{
+		if((num1 + num2) % 2 == 0 )
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	public boolean isEven2()
+	{
+		
 	}
 	
 }
